aaad25d28ed70dfc8ee739d2a821940fe35431ed
Matheus Gomes <tecmatheusg@hotmail.com>
2022-10-14 13:18:06 -0300

    Detalhes do 'seller' e eager loading

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMvc.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,7 +36,7 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(x => x.Id == id);
+            return _context.Seller.Include(obj => obj.Department). FirstOrDefault(x => x.Id == id);
         }
     }
 }
