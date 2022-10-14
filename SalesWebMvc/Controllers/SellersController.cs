c146a9d326f2c93c7094834360a239694db9491e
Matheus Gomes <tecmatheusg@hotmail.com>
2022-10-14 10:01:05 -0300

    'SellerService' e página 'Seller' criada

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using SalesWebMvc.Services;
+using System.Collections.Generic;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)   //injeçao dependencias
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerService.FindAll();                //controlador acessa model
+            return View(list);                                  //controlador retorna view
         }
     }
 }
