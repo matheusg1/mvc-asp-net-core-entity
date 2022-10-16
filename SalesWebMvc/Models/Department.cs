ad0df6b369815d183bf7a110c5efff7a40af37d4
Matheus Gomes <tecmatheusg@hotmail.com>
2022-10-15 23:18:59 -0300

    Implementado buscas agrupadas

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Cryptography;
 
@@ -19,7 +20,10 @@ namespace SalesWebMvc.Models
         }
 
         public int Id { get; set; }
+
+        [Display(Name = "Nome")]
         public string Name { get; set; }
+
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
         public void AddSeller(Seller seller)
